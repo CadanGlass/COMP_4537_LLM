85eefeaadd326825a8e75199f9beec0aa2b39dba
annettelam <113328885+annettelam@users.noreply.github.com>
2024-11-07 13:45:10 -0800

    Amend main py

--- main.py
+++ main.py
@@ -48,13 +48,13 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    # allow_origins=origins,
+    # allow_credentials=True,
+    # allow_methods=["*"],
+    # allow_headers=["*"],
     
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=[
         "Authorization",
         "Content-Type",
